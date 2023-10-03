a300a297898ecfa3b763a61cb5e3fdfd7c908e28
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-10-04 01:24:43 +0700

    Add Table Layout

---
+++ app/src/main/java/convert/csharp/to/firstproject/TableLayout.kt
@@ -0,0 +1,11 @@
+package convert.csharp.to.firstproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TableLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_layout)
+    }
+}
